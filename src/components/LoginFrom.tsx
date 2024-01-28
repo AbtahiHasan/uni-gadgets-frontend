516c10e09dbec01984e48fc92e177b98b953732f
AbtahiHasan <anikidpersonal@gmail.com>
2024-01-28 11:59:40 +0600

    login sestem done

--- src/components/LoginFrom.tsx
+++ src/components/LoginFrom.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { useState, useTransition } from "react";
-
+import { jwtDecode } from "jwt-decode";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { LoginSchema } from "@/schemas";
@@ -18,17 +19,24 @@ import { CardWrapper } from "./shared/card-wrapper";
 import { FormError } from "./shared/form-error";
 import { FormSuccess } from "./shared/form-success";
 import { Button } from "./ui/button";
+import { useLoginMutation } from "@/redux/features/auth/authApi";
+import { useAppDispatch } from "@/redux/hooks";
+import { setUser } from "@/redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 export const LoginFrom = () => {
   const [error, setError] = useState<string | undefined>("");
+
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
-
+  const navigate = useNavigate();
+  const [login] = useLoginMutation();
+  const dispatch = useAppDispatch();
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      email: "abtahihasan@gmail.com",
+      password: "123456",
     },
   });
 
@@ -37,21 +45,28 @@ export const LoginFrom = () => {
     setSuccess("");
     console.log(values);
     startTransition(() => {
-      //   login(values, callbackUrl)
-      //     .then((data) => {
-      //       if (data?.error) {
-      //         form.reset();
-      //         setError(data.error);
-      //       }
-      //       if (data?.success) {
-      //         form.reset();
-      //         setSuccess(data.success);
-      //       }
-      //       if (data?.twoFactor) {
-      //         setShowTwoFactor(true);
-      //       }
-      //     })
-      //     .catch(() => setError("Something went wrong"));
+      login(values)
+        .then((data: any) => {
+          console.log("login", data?.data?.data?.accessToken);
+          if (data?.data.error) {
+            form.reset();
+            setError(data.error);
+          }
+          if (data?.data?.success) {
+            form.reset();
+
+            const user = jwtDecode(data?.data?.data?.accessToken);
+            dispatch(
+              setUser({ user: user, token: data?.data?.data?.accessToken })
+            );
+            setSuccess(data?.data?.message);
+            navigate("/dashboard");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          setError("Something went wrong");
+        });
     });
   };
 
